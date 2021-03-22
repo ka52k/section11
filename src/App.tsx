7990b1452234d70f45f6a428d366d26fc8710657
ka52k <76809740+ka52k@users.noreply.github.com>
2021-03-22 14:32:44 +0900

    20210322－2変更

--- src/App.tsx
+++ src/App.tsx
@@ -2,23 +2,37 @@ import "./styles.css";
 import { UserCard } from "./components/UserCard";
 import axios from "axios";
 import { User } from "./types/api/user";
+import { UserProfile } from "./types/userProfile";
+import { useState } from "react";
 
-const user = {
-  id: 1,
-  name: "圭一",
-  email: "kk@kk.com",
-  address: "localhost:8001"
-}
+// const user = {
+//   id: 1,
+//   name: "圭一",
+//   email: "kk@kk.com",
+//   address: "localhost:8001"
+// }
 
 export default function App() {
+  const [userProfiles, setUserProfiles] = useState<Array<UserProfile>>([]);
+
   const onClickFetchUser = () => {
-    axios.get<Array<User>>("https://jsonplaceholder.typicode.com/users");
-  }
+    axios.get<Array<User>>("https://jsonplaceholder.typicode.com/users").then((res) => {
+      const data = res.data.map((user) => ({
+        id: user.id,
+        name: `${user.name}(${user.username})`,
+        email: user.email,
+        address: `${user.address.city}${user.address.suite}${user.address.street}`,
+      }));
+      setUserProfiles(data);
+    });
+  };
 
   return (
     <div className="App">
       <button onClick={onClickFetchUser}>データ取得</button>
-      <UserCard user={user} />
+      {userProfiles.map((user) => (
+        <UserCard key={user.id} user={user} />
+      ))}    
     </div>
   );
 }
