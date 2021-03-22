b9f94c898048ce4025be5032e641928b86a4ed1c
ka52k <76809740+ka52k@users.noreply.github.com>
2021-03-22 13:25:55 +0900

    20210322-1変更

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,24 @@
 import "./styles.css";
+import { UserCard } from "./components/UserCard";
+import axios from "axios";
+import { User } from "./types/api/user";
+
+const user = {
+  id: 1,
+  name: "圭一",
+  email: "kk@kk.com",
+  address: "localhost:8001"
+}
 
 export default function App() {
+  const onClickFetchUser = () => {
+    axios.get<Array<User>>("https://jsonplaceholder.typicode.com/users");
+  }
+
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <button onClick={onClickFetchUser}>データ取得</button>
+      <UserCard user={user} />
     </div>
   );
 }
